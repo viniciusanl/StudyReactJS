653e5a69f4015ecf23593767820afa7049db95b4
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-11-02 17:12:53 -0300

    commit 02-11-2018

--- src/App.js
+++ src/App.js
@@ -3,26 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+
+            <div className = "App" >
+
+            <header className = "App-header" >
+
+            <img src="https://scontent.fplu9-2.fna.fbcdn.net/v/t1.0-9/26166360_1143774309091022_3246020457323943336_n.jpg?_nc_cat=105&_nc_ht=scontent.fplu9-2.fna&oh=4f7df3faebdfb6bb469946a9837b59fd&oe=5C481747"
+            className = "App-MinhaImagem" align-content="right" title="Teste"/>
+            
+            </header>
+
+                <div className="App-Footer">
+                    <img src = {logo}className = "App-Logo" alt = "logo"/>
+                    <p> Vinícius Andrade Lopes </p>
+                </div>
+
+            </div>
+            
+        );
+    }
 }
 
 export default App;
