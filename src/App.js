a3a9f459ae37d24f77b1e9975acaa52ca58124e8
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-11-03 14:07:55 -0300

    commit 03/11/2018

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -11,13 +12,15 @@ class App extends Component {
             <header className = "App-header" >
 
             <img src="https://scontent.fplu9-2.fna.fbcdn.net/v/t1.0-9/26166360_1143774309091022_3246020457323943336_n.jpg?_nc_cat=105&_nc_ht=scontent.fplu9-2.fna&oh=4f7df3faebdfb6bb469946a9837b59fd&oe=5C481747"
-            className = "App-MinhaImagem" align-content="right" title="Teste"/>
-            
+            id="img-me" align-content="right" title="Vinicius Andrade Lopes"/>
+
+            <div></div>
+                        
             </header>
 
-                <div className="App-Footer">
+                <div id="footer">
                     <img src = {logo}className = "App-Logo" alt = "logo"/>
-                    <p> Vinícius Andrade Lopes </p>
+                    <p id="texto"> Vinícius Andrade Lopes </p>
                 </div>
 
             </div>
