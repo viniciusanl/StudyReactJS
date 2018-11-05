ff1ac68bfedfcff628dd0f2ccff835a2cec7ba4d
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-11-05 16:43:13 -0200

    commit 05/11/2018

--- src/App.js
+++ src/App.js
@@ -10,17 +10,10 @@ class App extends Component {
             <div className = "App" >
 
             <header className = "App-header" >
-
+            
             <img src="https://scontent.fplu9-2.fna.fbcdn.net/v/t1.0-9/26166360_1143774309091022_3246020457323943336_n.jpg?_nc_cat=105&_nc_ht=scontent.fplu9-2.fna&oh=4f7df3faebdfb6bb469946a9837b59fd&oe=5C481747"
-            id="img-me" align-content="right" title="Vinicius Andrade Lopes"/>
+            className="img-me" align-content="right" title="Vinicius Andrade Lopes"/>
             
-            
-            <div className="DBotao">
-                <button tipe="submit" className="Botao">Teste</button>
-            </div> 
-
-            <div></div>
-                        
             </header>
 
                 <div id="footer">
@@ -29,10 +22,14 @@ class App extends Component {
                 </div>
 
             </div>
+
             
         );
+
+        
     }
-    
 }
+
+
 
 export default App;
