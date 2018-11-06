186410c38f36bb573a49dacec4781ed7f47ac396
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-11-06 18:01:16 -0200

    commit 06/11/2018

--- src/App.js
+++ src/App.js
@@ -9,32 +9,49 @@ class App extends Component {
             <header className="App-header">
               <img src="https://scontent.fplu9-2.fna.fbcdn.net/v/t1.0-9/26166360_1143774309091022_3246020457323943336_n.jpg?_nc_cat=105&_nc_ht=scontent.fplu9-2.fna&oh=4f7df3faebdfb6bb469946a9837b59fd&oe=5C481747" className="img-me" align-content="" title="Vinicius Andrade Lopes" />
 
-              <h1 className="cor-titulo">&lt; 
-              <span className="titulo">
-                Vinícius Andrade Lopes
-              </span>
-              /&gt;</h1>
+              <h1 className="cor-titulo">
+                &lt;!--
+                <span className="titulo"> Vinícius Andrade Lopes </span>
+                --&gt; {/* &lt; = < | &gt; = >*/}
+              </h1>
 
-                <br></br>
+              <br />
 
-              <p className="texto">
-                Tenho 24 anos, sou estudante do curso de Sistemas de
-                Informação e fanático por tecnologia,{" "}
-              </p>
-              <p className = "texto">
-                procuro me desenvolver profissionalmente na área de TI
-                com a finalidade de alcançar meus objetivos e
-              </p>
-              <p className = "texto">
-                contribuir para o bom desempenho da empresa.
+              <p className="cor-texto">
+                /* &nbsp;
+                <span className="texto">
+                  Tenho 24 anos, sou estudante do curso de Sistemas de
+                  Informação e fanático por tecnologia,
+                </span>
+                <br />
+                <span className="texto">
+                  procuro me desenvolver profissionalmente na área de TI
+                  com a finalidade de alcançar meus objetivos e
+                </span>
+                <br />
+                <span className="texto">
+                  contribuir para o bom desempenho da empresa.
+                </span> &nbsp; /*
               </p>
             </header>
 
-            <div id="footer">
-                <div className="icons">
-                    <i class="fab fa-linkedin"></i>
-                </div>
+            {/* Footer dos Icones */}
+            <div className="footer-icon">
+              <a href="https://www.linkedin.com/in/vinicius-andrade-lopes/" target="_blank">
+                <img src="https://image.flaticon.com/icons/svg/1051/1051333.svg" className="icons" />
+              </a>
+              &nbsp;&nbsp; {/* &nbsp; - Espaçamento */}
+              <a href="https://github.com/viniciusanl" target="_blank">
+                <img src="https://image.flaticon.com/icons/svg/3/3721.svg" className="icons" />
+              </a>
+              &nbsp;&nbsp; {/* &nbsp; - Espaçamento */}
+              <a href="https://www.facebook.com/vinicius.anl" target="_blank">
+                <img src="https://image.flaticon.com/icons/svg/1051/1051309.svg" className="icons" />
+              </a>
+            </div>
 
+            {/* Footer dassinatura */}
+            <div className="footer">
               <img src={logo} className="App-Logo" alt="logo" />
             </div>
           </div>;  
@@ -42,3 +59,4 @@ class App extends Component {
 }
 
 export default App;
+
