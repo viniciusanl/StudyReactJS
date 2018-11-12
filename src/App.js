4b0ba9f1f5305b52b4c654afadc79b1cf6890a54
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2018-11-12 15:52:12 -0200

    commit 12/11/2018

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import linkedin from "../src/icon/linkedin-sign.png"
 import facebook from "../src/icon/facebook-logo.png"
 import github from "../src/icon/github-sign.png"
 import download from "../src/icon/archive.png"
+import instagram from "../src/icon/instagram.png"
 import './App.css';
 
 class App extends Component {
@@ -43,25 +44,37 @@ class App extends Component {
               {/* Footer dos Icones */}
               {/* target=blank abre a página em outra aba*/}
               <div className="footer-icon">
+
                 <a href="https://www.linkedin.com/in/vinicius-andrade-lopes/" target="_blank">
                   <img src={linkedin} className="icons" />
                 </a>
+
                 &nbsp;&nbsp; {/* &nbsp; - Espaçamento */}
                 <a href="https://github.com/viniciusanl" target="_blank">
                   <img src={github} className="icons" />
                 </a>
+
                 &nbsp;&nbsp; {/* &nbsp; - Espaçamento */}
                 <a href="https://www.facebook.com/vinicius.anl" target="_blank">
                   <img src={facebook} className="icons" />
                 </a>
+
+                &nbsp;&nbsp; {/* &nbsp; - Espaçamento */}
+                <a href="https://www.instagram.com/vinicius.anl/?hl=pt-br" target="_blank">
+                  <img src={instagram} className="icons"></img>
+                </a>
+
                 &nbsp;&nbsp; {/* &nbsp; - Espaçamento */}
                 <a href="https://drive.google.com/file/d/1YIgbj6sPt8zBjxtNla-iLOI2YKizldPx/view?usp=sharing" target="_blank">
                   <img src={download} className="icons" />
                 </a>
+
               </div>
+
             </header>
 
-            {/* Footer dassinatura */}
+
+            {/* Footer assinatura */}
           <div className="footer" href="http://localhost:3000/">
             <img src={logo} className="App-Logo" alt="logo" />
               {/*<p className = "txtfooter" >
