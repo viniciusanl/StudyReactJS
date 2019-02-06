7385cf5ffa53f1cc2b4e29d624ae6bbc31afdff6
Vinicius Andrade Lopes <30848464+viniciusanl@users.noreply.github.com>
2019-02-06 19:45:16 -0200

    commit 06/02/2019

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ import github from "../src/icon/github-sign.png"
 import download from "../src/icon/archive.png"
 import instagram from "../src/icon/instagram.png"
 import MinhaLogo from "../src/icon/Logo Vinicius 25-11-2018.png"
+import twitter from "../src/icon/twitter.png"
+import cv from "../src/icon/cv1.png"
 import './App.css';
 
 class App extends Component {
     render() {
-        return <div className="App">
+        return (
+          <div className="App">
             <header className="App-header">
               {/* Minha Imagem */}
-              <img src={MinhaLogo} className="img-me"/>
+              <img src={MinhaLogo} className="img-me" />
 
               <h1 className="cor-titulo">
                 &lt;!--
@@ -25,56 +28,73 @@ class App extends Component {
               <br />
 
               <p class="cor-texto">
-              &#123;/* 
+                &#123;/*
                 <span class="texto">
-                Um amante da música e entusiasta da tecnologia
+                  Um amante da música e entusiasta da tecnologia
                 </span>
                 <br />
                 <span class="texto">
-                buscando impulsionar seus conhecimentos e sabedoria.
+                  buscando impulsionar seus conhecimentos e sabedoria.
                 </span>
-               */&#125;
+                */&#125;
+                <div class="icon-all">
+                  <a
+                    href="https://drive.google.com/file/d/1tf18nnfJpon3ioUJcinvq07_AcTbtaFW/view"
+                    target="_blank"
+                  >
+                    <img src={cv} className="icon-cv" />
+                  </a>
+                </div>
               </p>
 
               {/* Footer dos Icones */}
               {/* target=blank abre a página em outra aba*/}
               <div className="footer-icon">
-
-                <a href="https://www.linkedin.com/in/vinicius-andrade-lopes/" target="_blank">
+                <a
+                  href="https://www.linkedin.com/in/vinicius-andrade-lopes/"
+                  target="_blank"
+                >
                   <img src={linkedin} className="icons" />
                 </a>
-
                 &nbsp; {/* &nbsp; - Espaçamento */}
-                <a href="https://github.com/viniciusanl" target="_blank">
+                <a
+                  href="https://github.com/viniciusanl"
+                  target="_blank"
+                >
                   <img src={github} className="icons" />
                 </a>
-
                 &nbsp; {/* &nbsp; - Espaçamento */}
-                <a href="https://www.facebook.com/vinicius.anl" target="_blank">
+                <a
+                  href="https://www.facebook.com/vinicius.anl"
+                  target="_blank"
+                >
                   <img src={facebook} className="icons" />
                 </a>
-
                 &nbsp; {/* &nbsp; - Espaçamento */}
-                <a href="https://www.instagram.com/vinicius.anl/?hl=pt-br" target="_blank">
-                  <img src={instagram} className="icons"></img>
+                <a
+                  href="https://www.instagram.com/vinicius.anl/?hl=pt-br"
+                  target="_blank"
+                >
+                  <img src={instagram} className="icons" />
                 </a>
-
                 &nbsp; {/* &nbsp; - Espaçamento */}
-              <a href="https://drive.google.com/file/d/1tf18nnfJpon3ioUJcinvq07_AcTbtaFW/view?usp=sharing" target="_blank">
-                  <img src={download} className="icons" />
+                <a
+                  href="https://twitter.com/viniciusalo"
+                  target="_blank"
+                >
+                  <img src={twitter} className="icons" />
                 </a>
-
               </div>
-
             </header>
 
             {/* Footer assinatura */}
-          <div className="footer">
-            <img src={logo} className="App-Logo" alt="logo" />
+            <div className="footer">
+              <img src={logo} className="App-Logo" alt="logo" />
               {/*<p className = "txtfooter" >
               <span> Vinicius Andrade Lopes </span></p>*/}
             </div>
-          </div>;  
+          </div>
+        );  
     }
 }
 
